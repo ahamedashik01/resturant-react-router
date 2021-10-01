514defe708a05c81be48fdbd994dc76421e906c7
ahamedashik01 <ashikshamim.007@gmail.com>
2021-10-01 19:17:58 +0600

    react-route and dynamic route system added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about page</h1>
+        <div className=" text-center my-5">
+            <p className="fs-1 fw-bold">ABOUT page is not live yet <br /> We are working on it</p>
+            <Link to="/home">Go back to home</Link>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Sorry page not found</h1>
+        <div className=" text-center my-5">
+            <p className="fs-1 fw-bold">404 error <br />
+                "Sorry page not found"</p>
+            <Link to="/home">Go back to home</Link>
         </div>
     );
 };
